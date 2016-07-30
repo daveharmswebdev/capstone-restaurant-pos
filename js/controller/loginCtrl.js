e801cf666e3f50f94e72a7eb44c4d8815b56d85a
Dave Harms <daveharmswebdev@gmail.com>
2016-07-30 10:32:15 -0500

    added some style to login screen, will test and develop valid logic for login screen

--- js/controller/loginCtrl.js
+++ js/controller/loginCtrl.js
@@ -2,10 +2,18 @@
 
 module.exports = function($scope, loginFactory) {
   $scope.signUp = function() {
-    console.log('sign in with', $scope.credentials);
-    loginFactory.createAccount($scope.credentials.email, $scope.credentials.password);
+    if (event.keyCode === 13) {
+      console.log('sign in with', event.type, event);
+    } else if (event.type === 'click') {
+      console.log(isValid($scope.credentials.email, $scope.credentials.password));
+      console.log('sign in with', event.type, event);
+    }
+    // loginFactory.createAccount($scope.credentials.email, $scope.credentials.password);
   };
   $scope.login = function() {
     console.log('login in with', $scope.credentials);
+  };
+  let isValid = function(email,password) {
+    return true;
   };
 };
