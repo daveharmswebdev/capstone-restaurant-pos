a7aeb6fb7bcbf86c384727a760f1af6a096fef05
Dave Harms <daveharmswebdev@gmail.com>
2016-08-01 06:12:50 -0500

    able to get profile with uid, able to create profile with uid and email keys

--- js/factory/profileFactory.js
+++ js/factory/profileFactory.js
@@ -2,15 +2,45 @@
 
 let FBCreds = require('../values/firebaseCreds');
 
-module.exports = function($http) {
+module.exports = function($q, $http, loginFactory) {
 
   let submitProfile = function(profile) {
-    console.log(profile);
-    console.log(FBCreds.databaseURL);
+    profile.email = loginFactory.getCurrentUser().email;
+    profile.uid = loginFactory.getCurrentUser().uid;
+    return $q(function(resolve, reject) {
+      $http.post(`${FBCreds.databaseURL}/profile.json`, JSON.stringify(profile))
+      .success(function(response) {
+        resolve(response);
+      })
+      .error(function(error) {
+        reject(error);
+      });
+    });
   };
 
-  let getProfile = function() {
-    console.log('getting profile');
+  // let postNewItems = function(newItem) {
+  //   console.log(newItem, FBCreds.dbURL);
+  //   return $q(function(resolve, reject) {
+  //     $http.post(`${FBCreds.dbURL}/items.json`, JSON.stringify(newItem))
+  //     .success(function(objFromFirebase) {
+  //       resolve(objFromFirebase);
+  //     })
+  //     .error(function(error) {
+  //       reject(error);
+  //     });
+  //   });
+  // };
+
+  let getProfile = function(uid) {
+    return $q(function(resolve, reject) {
+      $http.get(`${FBCreds.databaseURL}/profile.json?orderBy="uid"&equalTo="${uid}"`)
+      .success(function(results) {
+        resolve(results);
+      })
+      .error(function(error) {
+        reject(error);
+      });
+    });
   };
 
   return {
