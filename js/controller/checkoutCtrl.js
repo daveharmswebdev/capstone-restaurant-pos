917c6cf097d536e435568a5acb28402285e0b241
Dave Harms <daveharmswebdev@gmail.com>
2016-08-01 18:44:58 -0500

    created a checkout form

--- js/controller/checkoutCtrl.js
+++ js/controller/checkoutCtrl.js
@@ -1,5 +1,14 @@
 'use strict';
 
-module.exports = function() {
-
+module.exports = function($scope, orderFactory) {
+  console.log('checout ctrl');
+  console.log(orderFactory.getCurrentTicket());
+  // $scope.ticket = orderFactory.getCurrentTicket();
+  $scope.ticket = [
+    { name: "Pad Thai", price: 9},
+    { name: "Fried Rice", price: 8},
+    { name: "Kao Prow", price: 9},
+    { name: "Tom Kka Gai", price: 7},
+    { name: "Spring Rolls", price: 5}
+  ];
 };
