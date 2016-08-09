617bda34c768117b5a730c710d78d67cbd3112cb
Dave Harms <daveharmswebdev@gmail.com>
2016-08-09 09:53:27 -0500

    able to sort report and add timestampstring

--- js/controller/eodCtrl.js
+++ js/controller/eodCtrl.js
@@ -4,28 +4,49 @@
 
 module.exports = function($scope, reportFactory) {
   $scope.orders = [];
-  $scope.eodTotal = 0;
+  $scope.eodGrandTotal = 0;
+  $scope.eodSubTotal = 0;
+  $scope.eodTax = 0;
   $scope.eodTotalItems = 0;
+  $scope.eodDate = null;
 
   reportFactory.getOrders()
   .then(function(results) {
     let keys = Object.keys(results);
     keys.forEach(key => $scope.orders.push(results[key]));
     // $scope.orders = $scope.orders.map(order => order.timestamp = new Date(order.timestamp))
-    console.log($scope.orders[0]);
+    sortOrders();
     getReports();
-    $scope.eodTotal = $scope.eodTotal.toFixed(2);
-    console.log($scope.eodTotal, $scope.eodTotalItems);
+    $scope.eodGrandTotal = parseFloat($scope.eodGrandTotal.toFixed(2));
+    $scope.eodSubTotal = parseFloat($scope.eodSubTotal.toFixed(2));
+    $scope.eodTax = parseFloat($scope.eodTax.toFixed(2));
+    $scope.orders.forEach(function(order) {
+      let timeStampString = new Date(order.timestamp);
+      console.log(timeStampString.toString());
+      order.timeStampString = timeStampString.toString();
+    });
   })
   .catch(function(error) {
     console.log(error);
   });
 
+  function sortOrders() {
+    $scope.orders.sort(function(a, b) {
+      if (a.timestamp > b.timestamp) {
+        return 1;
+      } else if (a.timestamp < b.timestamp) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
   function getReports() {
     $scope.orders.forEach(function(order) {
-      $scope.eodTotal += order.grandTotal;
+      $scope.eodGrandTotal += order.grandTotal;
+      $scope.eodSubTotal += order.subtotal;
+      $scope.eodTax += order.tax;
       $scope.eodTotalItems += order.order.length;
-      // eodTotalItems = orders.order.length;
     });
   }
 };
