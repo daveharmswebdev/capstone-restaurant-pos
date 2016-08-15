59c250afe4ac116fe257e8b10a0f755f5c1c6713
Dave Harms <daveharmswebdev@gmail.com>
2016-08-15 11:01:47 -0500

    modularized getData function into get mockCust, and getMockOrder

--- js/factory/mockDataFactory.js
+++ js/factory/mockDataFactory.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const cust = require('./mockCustomers');
+
 let mock = {};
 
 function getHours() {
@@ -8,15 +10,28 @@ function getHours() {
   return hours[n];
 }
 
+function getMockOrder() {
+  let mockOrder = {};
+  let mockCust = cust.getCustomer();
+  mockOrder.city = 'Nashville';
+  mockOrder.delivery = Math.random() > 0.4 ? true : false;
+  mockOrder.firstName = mockCust.firstName;
+  mockOrder.lastName = mockCust.lastName;
+  mockOrder.email = mockCust.email;
+  mockOrder.state = 'TN';
+  mockOrder.status = Math.random() > 0.95 ? 'cancelled' : 'complete';
+  return mockOrder;
+}
+
 mock.getData = function(mockDate) {
   let tempDate = new Date(mockDate);
-  console.log('start', tempDate);
-  for (let x = 0; x < 5; x++) {
+  for (let x = 0; x < 1; x++) {
     tempDate.setDate(tempDate.getDate() + 1);
-    let customers = Math.floor(Math.random() * 10) + 25;
+    let customers = Math.floor(Math.random() * 5) + 1;
     for (let i = 0; i < customers; i++) {
       tempDate.setHours(getHours(), Math.floor(Math.random() * 60), Math.floor(Math.random() * 60));
-      console.log(x, tempDate, customers);
+      let mockOrder = getMockOrder(tempDate);
+      console.log(mockOrder);
     }
   }
 };
